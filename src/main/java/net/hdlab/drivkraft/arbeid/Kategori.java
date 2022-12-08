81b7143d26e2ceb4b966aa299740c902ebc4c2e7
Thor Hovden <thor.hovden@gmail.com>
2022-12-09 00:36:58 +0100

    Kategori

--- src/main/java/net/hdlab/drivkraft/arbeid/Kategori.java
+++ src/main/java/net/hdlab/drivkraft/arbeid/Kategori.java
@@ -1,7 +1,9 @@
 package net.hdlab.drivkraft.arbeid;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
 // Brukte mal fra https://github.com/spring-projects/spring-data-examples/blob/main/r2dbc/example/src/main/java/example/springdata/r2dbc/basics/Customer.java
+@Entity
 public record Kategori(@Id Integer id, String navn) {
 }
--- src/main/java/net/hdlab/drivkraft/arbeid/KategoriRepo.java
+++ src/main/java/net/hdlab/drivkraft/arbeid/KategoriRepo.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 // Brukte mal fra eget prosjekt d-emographics (som er basert på Spring Data Rest eksempel)
-@RepositoryRestResource(collectionResourceRel="kategori", path="arbeid/kategorier")
+@RepositoryRestResource(collectionResourceRel="kategori", path="kategorier")
 public interface KategoriRepo extends JpaRepository<Kategori, Integer> {
 
 }
